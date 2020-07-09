82f600330417f7f2a6ffc9c58add34204624d7af
yagodml <yagoskor@gmail.com>
2020-07-09 18:51:53 -0300

    api service

---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTk0NDA0MDcwLCJqdGkiOiI3ODA0YmU2Y2VhNjM0MjY4YTY5NGRmMGQ5YTFmZTgyYiIsInVzZXJfaWQiOjF9.YQMeh5dKf9zNRKSMfBWXhoNKMWIKgJzmL8ImZa19NOs'
+
+const proxy = 'https://cors-anywhere.herokuapp.com/'
+const url = 'http://volare-teste-front.herokuapp.com/api'
+
+export default axios.create({
+  baseURL: proxy + url,
+  headers: {
+    Authorization: `Bearer ${token}`,
+    'Content-Type': 'application/json;charset=utf-8',
+    'Access-Control-Allow-Origin': '*'
+  }
+})
