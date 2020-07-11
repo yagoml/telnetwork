fe05ed428236ceed84a2874e288005432abb3043
yagodml <yagoskor@gmail.com>
2020-07-11 19:51:58 -0300

    check route breadcrumb

--- src/views/CheckRoute/index.jsx
+++ src/views/CheckRoute/index.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import CheckRouteForm from '../../components/check-route/CheckRouteForm'
 import RouteWaypoints from '../../components/check-route/RouteWaypoints'
+import { Breadcrumb } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 export default function CheckRoute() {
+  const renderBreadcrumb = () => {
+    return (
+      <Breadcrumb>
+        <Breadcrumb.Item href="#">
+          <Link to="/">Dashboard</Link>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item active>Consulta de Conectividade</Breadcrumb.Item>
+      </Breadcrumb>
+    )
+  }
+
   return (
     <div className="check-route">
       <h2 className="mt-3">Consulta de Conectividade</h2>
+      {renderBreadcrumb()}
       <CheckRouteForm />
       <RouteWaypoints />
     </div>
